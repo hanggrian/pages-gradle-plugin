011a4e0751320d0889b6233e46ad1c638d005e4d
Hendra Anggrian <hendraanggrian@gmail.com>
2022-12-06 07:10:07 +0700

    Remove old main module, add column limit to 80, add `Config` suffix to KTS functions used with double colon operator

--- pages-gradle-plugin/src/main/kotlin/com/hendraanggrian/pages/PageButton.kt
+++
@@ -1,9 +0,0 @@
-package com.hendraanggrian.pages
-
-/**
- * Button data class.
- *
- * @param text button text.
- * @param url absolute or relative path to redirect to on button click.
- */
-data class PageButton(val text: String, val url: String)
